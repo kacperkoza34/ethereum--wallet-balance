5bcda2f894e8973f33485a890708c4e8767e10c4
kacperkoza34 <kacperkoza34@gmail.com>
2024-11-18 18:37:49 +0100

    feat: add transfer logic

--- src/utils/store/services/contracts/contracts.actions.ts
+++ src/utils/store/services/contracts/contracts.actions.ts
@@ -5,6 +5,10 @@ import { erc20BalanceOf } from '@/utils/web3/erc-20-balance-of';
 import { erc20Decimals } from '@/utils/web3/erc-20-decimals';
 import { erc20Symbol } from '@/utils/web3/erc-20-symbol';
 import { type TokeData } from '@/utils/store/services/contracts/contracts.slice';
+import {
+  erc20Transfer,
+  type Erc20TransferParams,
+} from '@/utils/web3/erc-20-transfer';
 
 export const BALANCE_OF_ACTION_NAME = 'contracts/getBalance';
 
@@ -38,4 +42,16 @@ export const getErc20Data = createAsyncThunk<
     symbol,
     tokenType: { address: contractAddress, type: 'erc20' },
   };
+});
+
+export const TRANSFER_ACTION_NAME = 'contracts/transfer';
+
+export const transferErc20Token = createAsyncThunk<
+  unknown,
+  Erc20TransferParams,
+  { state: RootState }
+>(TRANSFER_ACTION_NAME, async (args) => {
+  const signer = getSigner();
+  const contractAddress = import.meta.env.VITE_ERC_20_ADDRESS as string;
+  await erc20Transfer({ ...args, signer, contractAddress });
 });
